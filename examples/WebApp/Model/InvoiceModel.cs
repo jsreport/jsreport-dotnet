8c2d461852f462b9bc1c6b939e7f196e80f5dc24
pofider <jan.blaha@hotmail.com>
2017-07-12 18:02:47 +0200

    initial commit

---
+++ examples/WebApp/Model/InvoiceModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Model
+{
+    public class InvoiceModel
+    {
+        public string Number { get; set; }
+        public CompanyModel Seller { get; set; }
+        public CompanyModel Buyer { get; set; }
+        public IEnumerable<ItemModel> Items { get; set; }
+
+        public static InvoiceModel Example()
+        {
+            return new InvoiceModel()
+            {
+                Number = "123",
+                Seller = new CompanyModel()
+                {
+                    Name = "Next Step Webs, Inc.",
+                    Road = "12345 Sunny Road",
+                    Country = "Sunnyville, TX 12345"
+                },
+                Buyer = new CompanyModel()
+                {
+                    Name = "Acme Corp.",
+                    Road = "16 Johnson Road",
+                    Country = "Paris, France 8060"
+                },
+                Items = new List<ItemModel>()
+                {
+                    new ItemModel()
+                    {
+                        Name = "Website design",
+                        Price = 300
+                    },
+                    new ItemModel()
+                    {
+                        Name = "Implementing specific components",
+                        Price = 600
+                    },
+                    new ItemModel()
+                    {
+                        Name = "Maintenance and support",
+                        Price = 150
+                    }
+                }
+            };
+        }
+    }
+
+    public class CompanyModel
+    {
+        public string Name { get; set; }
+        public string Road { get; set; }
+        public string Country { get; set; }
+    }
+
+    public class ItemModel
+    {
+        public string Name { get; set; }
+        public long Price { get; set; }
+    }
+}
---
+++ examples/WebApp/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using jsreport.MVC;
+using jsreport.Local;
+
+namespace WebApp
+{
+    public class Startup
+    {        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddJsReport(new LocalReporting().AsUtility().Create());
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseStaticFiles();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }    
+    }
+}
